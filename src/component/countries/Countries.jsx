ae3f5e1f5aacef133a21d5dba310a00b9cef4da1
almahmudzihad <amzihad@gmail.com>
2026-03-15 13:15:51 +0600

    grid style css

--- src/component/countries/Countries.jsx
+++ src/component/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use, useState } from 'react';
 import Country from '../country/Country';
+import './countries.css';
 
 const Countries = ({countryPromis}) => {
     
@@ -9,10 +10,13 @@ const Countries = ({countryPromis}) => {
     return (
         <div>
             <h3>All countries : {countries.length}</h3>
-            
+            <div className='card-container'>   
+                
             {
                 countries.map(country => <Country country={country} key={country.cca3.cca3}></Country>)
             }
+            </div>
+            
             
         </div>
     );
